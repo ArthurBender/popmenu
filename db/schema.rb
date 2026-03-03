f67767b55e3b13941c562208a194f384c8dff2f5
Arthur Bender <arthurllbender@gmail.com>
2026-03-03 16:17:51 -0300

    Fix: reload schema

--- db/schema.rb
+++ db/schema.rb
@@ -17,15 +17,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_03_182905) do
   create_table "menu_items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.bigint "menu_id", null: false
-    t.string "name"
-    t.float "price"
+    t.string "name", null: false
+    t.float "price", null: false
     t.datetime "updated_at", null: false
     t.index ["menu_id"], name: "index_menu_items_on_menu_id"
   end
 
   create_table "menus", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.string "name"
+    t.string "name", null: false
     t.datetime "updated_at", null: false
   end
 
