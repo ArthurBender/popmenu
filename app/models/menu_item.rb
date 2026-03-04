e68aa5aaaea793f53cabc5c94850c4456d7f9756
Arthur Bender <arthurllbender@gmail.com>
2026-03-04 10:05:04 -0300

    Feat: update models to match new schema

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,4 +1,7 @@
 class MenuItem < ApplicationRecord
   has_many :menu_entries, dependent: :destroy
   has_many :menus, through: :menu_entries
+
+  validates :name, :price, presence: true
+  validates :name, uniqueness: true
 end
