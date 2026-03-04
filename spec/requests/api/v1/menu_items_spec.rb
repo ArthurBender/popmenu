08ff51a3a28c92a93e8b0cd305f696fab82a32e6
Arthur Bender <arthurllbender@gmail.com>
2026-03-04 10:39:50 -0300

    Fix: update requests tests for new schema

--- spec/requests/api/v1/menu_items_spec.rb
+++ spec/requests/api/v1/menu_items_spec.rb
@@ -3,21 +3,33 @@ require 'rails_helper'
 RSpec.describe "Api::V1::MenuItems", type: :request do
   before do
     @menu = create(:menu)
-    @menu_item = create(:menu_item, menu: @menu)
+    @menu_item = create(:menu_item, menus: [ @menu ])
   end
 
   describe "index" do
     it "returns a list of menu items" do
-      get api_v1_menu_menu_items_path(menu_id: @menu.id)
+      get api_v1_restaurant_menu_menu_items_path(restaurant_id: @menu.restaurant_id, menu_id: @menu.id)
 
       expect(response).to have_http_status(200)
       expect(JSON.parse(response.body).length).to eq(1)
+    end
+
+    it "returns a 404 if the menu does not exist" do
+      get api_v1_restaurant_menu_menu_items_path(restaurant_id: @menu.restaurant_id, menu_id: 0)
+
+      expect(response).to have_http_status(404)
+    end
+
+    it "returns a 404 if the restaurant does not exist" do
+      get api_v1_restaurant_menu_menu_items_path(restaurant_id: 0, menu_id: @menu.id)
+
+      expect(response).to have_http_status(404)
     end
   end
 
   describe "show" do
     it "returns a single menu item" do
-      get api_v1_menu_menu_item_path(menu_id: @menu.id, id: @menu_item.id)
+      get api_v1_restaurant_menu_menu_item_path(restaurant_id: @menu.restaurant_id, menu_id: @menu.id, id: @menu_item.id)
 
       expect(response).to have_http_status(200)
       expect(JSON.parse(response.body)["name"]).to eq(@menu_item.name)
@@ -25,7 +37,7 @@ RSpec.describe "Api::V1::MenuItems", type: :request do
     end
 
     it "returns a 404 if the menu item does not exist" do
-      get api_v1_menu_menu_item_path(menu_id: @menu.id, id: 0)
+      get api_v1_restaurant_menu_menu_item_path(restaurant_id: @menu.restaurant_id, menu_id: @menu.id, id: 0)
 
       expect(response).to have_http_status(404)
     end
@@ -33,7 +45,7 @@ RSpec.describe "Api::V1::MenuItems", type: :request do
     it "doesn't return items from other menus" do
       other_menu = create(:menu)
 
-      get api_v1_menu_menu_item_path(menu_id: other_menu.id, id: @menu_item.id)
+      get api_v1_restaurant_menu_menu_item_path(restaurant_id: other_menu.restaurant_id, menu_id: other_menu.id, id: @menu_item.id)
 
       expect(response).to have_http_status(404)
     end
--- spec/requests/api/v1/menus_spec.rb
+++ spec/requests/api/v1/menus_spec.rb
@@ -7,25 +7,43 @@ RSpec.describe "Api::V1::Menus", type: :request do
 
   describe "index" do
     it "returns a list of menus" do
-      get api_v1_menus_path
+      get api_v1_restaurant_menus_path(restaurant_id: @menu.restaurant_id)
 
       expect(response).to have_http_status(200)
       expect(JSON.parse(response.body).length).to eq(1)
+    end
+
+    it "returns a 404 if the restaurant does not exist" do
+      get api_v1_restaurant_menus_path(restaurant_id: 0)
+
+      expect(response).to have_http_status(404)
     end
   end
 
   describe "show" do
     it "returns a single menu" do
-      get api_v1_menu_path(@menu)
+      get api_v1_restaurant_menu_path(restaurant_id: @menu.restaurant_id, id: @menu)
 
       expect(response).to have_http_status(200)
       expect(JSON.parse(response.body)["name"]).to eq(@menu.name)
     end
 
     it "returns a 404 if the menu does not exist" do
-      get api_v1_menu_path(0)
+      get api_v1_restaurant_menu_path(restaurant_id: @menu.restaurant_id, id: 0)
 
       expect(response).to have_http_status(404)
+    end
+
+    it "returns a 404 if the restaurant does not exist" do
+      get api_v1_restaurant_menu_path(restaurant_id: 0, id: @menu)
+
+      expect(response).to have_http_status(404)
+    end
+
+    it "does not return menus from other restaurants" do
+      other_restaurant = create(:restaurant)
+
+      get api_v1_restaurant_menu_path(restaurant_id: other_restaurant.id, id: @menu)
     end
   end
 end
