bb1ccb5e3fc82af88d94eac6a5ef7998e9fb8fc7
Arthur Bender <arthurllbender@gmail.com>
2026-03-04 12:25:05 -0300

    Feat: add route for import tool

---
+++ app/controllers/api/v1/imports_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::ImportsController < ApplicationController
+  def process
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      post "imports", to: "imports#process"
+
       resources :restaurants, only: %i[index show] do
         resources :menus, only: %i[index show] do
           resources :menu_items, only: %i[index show]
