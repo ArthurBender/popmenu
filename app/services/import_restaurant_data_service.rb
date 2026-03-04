b658a512bd11bd7525d1691fcf274391ef9d267a
Arthur Bender <arthurllbender@gmail.com>
2026-03-04 14:55:12 -0300

    Feat: added logs to conversion tool service

--- app/services/import_restaurant_data_service.rb
+++ app/services/import_restaurant_data_service.rb
@@ -11,9 +11,13 @@ class ImportRestaurantDataService
   end
 
   def call
+    log("Started process.")
+
     data = JSON.parse(@file.read)
+    log("File parsed.")
 
     validate_structure!(data)
+    log("File schema validated.")
 
     initial_data = count_data
 
@@ -42,20 +46,28 @@ class ImportRestaurantDataService
   end
 
   def import_restaurant(restaurant_data)
-    restaurant = Restaurant.find_or_create_by!(name: restaurant_data["name"])
+    restaurant = Restaurant.find_or_create_by!(name: restaurant_data["name"]) do
+      log("Created restaurant #{restaurant_data["name"]}.")
+    end
 
     restaurant_data["menus"].each do |menu_data|
-      menu = restaurant.menus.find_or_create_by!(name: menu_data["name"])
+      menu = restaurant.menus.find_or_create_by!(name: menu_data["name"]) do
+        log("Created menu #{menu_data["name"]}.")
+      end
 
       (menu_data["menu_items"] || menu_data["dishes"]).each do |item_data|
-        menu_item = MenuItem.find_or_create_by!(name: item_data["name"])
+        menu_item = MenuItem.find_or_create_by!(name: item_data["name"]) do
+          log("Created item #{item_data["name"]}.")
+        end
 
         begin
           menu.menu_entries.find_or_create_by!(menu_item: menu_item) do |menu_entry|
             menu_entry.price = item_data["price"]
+            log("Added entry for #{item_data["name"]} with price #{item_data["price"]} to menu #{menu_data["name"]}.")
           end
-        rescue ActiveRecord::RecordInvalid
+        rescue ActiveRecord::RecordInvalid => e
           @errors += 1
+          log("Error adding entry for #{item_data["name"]} with price #{item_data["price"]} to menu #{menu_data["name"]}: #{e.message}.", true)
         end
       end
     end
@@ -69,5 +81,15 @@ class ImportRestaurantDataService
     end
 
     result
+  end
+
+  def log(message, error = false)
+    message = "[Conversion Tool] #{message}"
+    if error
+      Rails.logger.error(message)
+      return
+    end
+
+    Rails.logger.info(message)
   end
 end
