6dc54ea6ae10872dd1e94253ab1eee7ef14f2bcf
Arthur Bender <arthurllbender@gmail.com>
2026-03-05 14:22:25 -0300

    Feat: add test for multiple imports with same file.

--- spec/services/import_restaurant_data_service_spec.rb
+++ spec/services/import_restaurant_data_service_spec.rb
@@ -26,6 +26,38 @@ RSpec.describe ImportRestaurantDataService do
       expect(result["logs"].size).to eq(8)
     end
 
+    it "is idempotent when importing the same file twice" do
+      first_file = File.open(Rails.root.join("spec/fixtures/files/imports_valid.json"))
+      second_file = File.open(Rails.root.join("spec/fixtures/files/imports_valid.json"))
+
+      first_result = described_class.call(first_file)
+      second_result = nil
+
+      expect {
+        second_result = described_class.call(second_file)
+      }.to change(Restaurant, :count).by(0)
+        .and change(Menu, :count).by(0)
+        .and change(MenuItem, :count).by(0)
+        .and change(MenuEntry, :count).by(0)
+
+      expect(first_result["totals"]).to eq({
+        "restaurants" => 2,
+        "menus" => 4,
+        "menu_items" => 7,
+        "menu_entries" => 8,
+        "errors" => 0
+      })
+      expect(second_result["totals"]).to eq({
+        "restaurants" => 0,
+        "menus" => 0,
+        "menu_items" => 0,
+        "menu_entries" => 0,
+        "errors" => 0
+      })
+      expect(second_result["success"]).to eq(true)
+      expect(second_result["logs"].size).to eq(8)
+    end
+
     it "raises for the invalid file" do
       file = File.open(Rails.root.join("spec/fixtures/files/imports_invalid.json"))
 
